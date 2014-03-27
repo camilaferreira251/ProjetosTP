92ea6070baa202ad98d3215a3b2b9c7885696793
Jads <jadsvictor@hotmail.com>
2014-03-27 00:26:31 -0300

    insert free software header in AprenderNumero

--- ABCDario/src/jogo/AprenderNumero.java
+++ ABCDario/src/jogo/AprenderNumero.java
@@ -1,3 +1,26 @@
+/*Classe AprenderNumero
+	 * Abcdário is free software: you can redistribute it and / or
+
+     modify it under the terms of the GNU General Public License as
+
+     published by the Free Software Foundation (FSF), version 2 of the
+
+     License, or (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+
+     but WITHOUT ANY WARRANTY; without even the implied warranty of fitness for any
+
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. View
+
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+
+     along with this program, if not, write to the Foundation of Software
+
+     Free (FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA */
+
 package jogo;
 
 import fga.mds.abcdario.R;
