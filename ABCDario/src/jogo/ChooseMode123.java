ea10292ff3cb1b294721d557cd635866deb2e119
Jads <jadsvictor@hotmail.com>
2014-03-28 19:18:20 -0300

    Deleting classes and ajustig some headers

--- ABCDario/src/jogo/ChooseMode123.java
+++ ABCDario/src/jogo/ChooseMode123.java
@@ -1,4 +1,4 @@
-	/*Classe ChooseMode123
+/*Classe ChooseMode123
 	 * Abcd�rio is free software: you can redistribute it and / or
 
      modify it under the terms of the GNU General Public License as
--- ABCDario/src/jogo/FirstActivity.java
+++ ABCDario/src/jogo/FirstActivity.java
@@ -1,4 +1,4 @@
-	/*Classe FirstActivity
+/*Classe FirstActivity
 	 * Abcd�rio is free software: you can redistribute it and / or
 
      modify it under the terms of the GNU General Public License as
