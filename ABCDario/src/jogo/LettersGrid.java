bb24f755ec44a616213ea99aa4c23a91b631b51b
Jads <jadsvictor@hotmail.com>
2014-03-28 20:49:15 -0300

    Stile and Desing

--- ABCDario/src/jogo/LettersGrid.java
+++ ABCDario/src/jogo/LettersGrid.java
@@ -1,5 +1,5 @@
 /*Classe LettersGrid
-	 * Abcdário is free software: you can redistribute it and / or
+ * Abcdário is free software: you can redistribute it and / or
 
      modify it under the terms of the GNU General Public License as
 
@@ -32,49 +32,48 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.GridLayout.LayoutParams;
 
-public class LettersGrid extends BaseAdapter{
+public class LettersGrid extends BaseAdapter {
 
 	private Context contexto;
-	
-	//Iniciando botoes na grade
-    public Integer[] botoesLetras = {
-   		R.drawable.bt_a,R.drawable.bt_b,R.drawable.bt_c,R.drawable.bt_d,R.drawable.bt_e,
-   		R.drawable.bt_f,R.drawable.bt_g,R.drawable.bt_h,R.drawable.bt_i,R.drawable.bt_j,
-   		R.drawable.bt_k,R.drawable.bt_l,R.drawable.bt_m,R.drawable.bt_n,R.drawable.bt_o,
-   		R.drawable.bt_p,R.drawable.bt_q,R.drawable.bt_r,R.drawable.bt_s,R.drawable.bt_t,
-   		R.drawable.bt_u,R.drawable.bt_v,R.drawable.bt_x,R.drawable.bt_z,R.drawable.bt_w,
-   		R.drawable.bt_y,
-    };
-    
-    public LettersGrid(Context c){
-    	this.contexto = c;
-    }
-	
+
+	// Iniciando botoes na grade
+	public Integer[] botoesLetras = { R.drawable.bt_a, R.drawable.bt_b,
+			R.drawable.bt_c, R.drawable.bt_d, R.drawable.bt_e, R.drawable.bt_f,
+			R.drawable.bt_g, R.drawable.bt_h, R.drawable.bt_i, R.drawable.bt_j,
+			R.drawable.bt_k, R.drawable.bt_l, R.drawable.bt_m, R.drawable.bt_n,
+			R.drawable.bt_o, R.drawable.bt_p, R.drawable.bt_q, R.drawable.bt_r,
+			R.drawable.bt_s, R.drawable.bt_t, R.drawable.bt_u, R.drawable.bt_v,
+			R.drawable.bt_x, R.drawable.bt_z, R.drawable.bt_w, R.drawable.bt_y, };
+
+	public LettersGrid(Context c) {
+		this.contexto = c;
+	}
+
 	@Override
 	public int getCount() {
-        return botoesLetras.length; 
+		return botoesLetras.length;
 	}
 
 	@Override
 	public Object getItem(int posicao) {
-    	return botoesLetras[posicao]; 
+		return botoesLetras[posicao];
 	}
 
 	@Override
 	public long getItemId(int posicao) {
 		return 0;
 	}
-	
+
 	@Override
 	public View getView(int posicao, View convertView, ViewGroup parent) {
-		
+
 		ImageView imageView = new ImageView(contexto);
-        imageView.setImageResource(botoesLetras[posicao]);
-        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-        imageView.setLayoutParams(new GridView.LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT));
-        
-        return imageView;
+		imageView.setImageResource(botoesLetras[posicao]);
+		imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+		imageView.setLayoutParams(new GridView.LayoutParams(
+				LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+
+		return imageView;
 	}
 
-	
 }
