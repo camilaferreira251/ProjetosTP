13dca66d88b2a856d5971b99707e04acaaaccdb1
Jads <jadsvictor@hotmail.com>
2014-03-28 21:18:21 -0300

    Stile and Desing

--- RNA/src/RNA/NeuronBackPropagation.java
+++ RNA/src/RNA/NeuronBackPropagation.java
@@ -25,34 +25,34 @@ package RNA;
 
 class NeuronBackPropagation {
 
-    private double[] deltaw = null;
-    private double deltaBias = 0;
+	private double[] deltaw = null;
+	private double deltaBias = 0;
 
-    NeuronBackPropagation(int tamanho) {
-        deltaw = new double[tamanho];
+	NeuronBackPropagation(int tamanho) {
+		deltaw = new double[tamanho];
 
-        for (int i = 0; i < deltaw.length; i++) {
-            deltaw[i] = 0;
-        }
-    }
+		for (int i = 0; i < deltaw.length; i++) {
+			deltaw[i] = 0;
+		}
+	}
 
-    final double getDeltaw(int i) {
-        return deltaw[i];
-    }
+	final double getDeltaw(int i) {
+		return deltaw[i];
+	}
 
-    final void setDeltaw(int i, double valor) {
-        deltaw[i] = valor;
-    }
+	final void setDeltaw(int i, double valor) {
+		deltaw[i] = valor;
+	}
 
-    final double getDeltaBias() {
-        return deltaBias;
-    }
+	final double getDeltaBias() {
+		return deltaBias;
+	}
 
-    final void setDeltaBias(double deltaBias) {
-        this.deltaBias = deltaBias;
-    }
+	final void setDeltaBias(double deltaBias) {
+		this.deltaBias = deltaBias;
+	}
 
-    int getTamanho() {
-        return deltaw.length;
-    }
+	int getTamanho() {
+		return deltaw.length;
+	}
 }
