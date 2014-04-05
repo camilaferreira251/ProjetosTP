4cd15808a579e619119caba03dd0b56fd8760ee7
unknown <camilaferreira251@gmail.com>
2014-04-05 11:50:56 -0300

    tranlation variables

--- ABCDario/src/jogo/GameController.java
+++ ABCDario/src/jogo/GameController.java
@@ -33,20 +33,20 @@ public class GameController {
 
 	private GamePersistence acessar = GamePersistence.getInstance();
 	
-	private String lista_num[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
-	private String lista_letra[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+	private String list_numbers[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+	private String list_letters[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
 			"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 	
 	public GameController(){}
 	
-	List<String> numero = new ArrayList<String>(Arrays.asList(lista_num));
-	List<String> letra = new ArrayList<String>(Arrays.asList(lista_letra));
+	List<String> number = new ArrayList<String>(Arrays.asList(list_numbers));
+	List<String> letter = new ArrayList<String>(Arrays.asList(list_letters));
 	
 	public int[] getModelNumero(){
 		
-		Collections.shuffle(numero);
+		Collections.shuffle(number);
 		
-		GameModel modelo = acessar.getModelNumero((String) numero.get(0));
+		GameModel modelo = acessar.getModelNumero((String) number.get(0));
 		
 		int[] id = new int[2];
 		
@@ -58,18 +58,18 @@ public class GameController {
 	
 	public int getIdBotaoNumero(){
 		
-		Collections.shuffle(numero);
+		Collections.shuffle(number);
 		
-		GameModel modelo =  acessar.getModelNumero((String) numero.get(0));
+		GameModel modelo =  acessar.getModelNumero((String) number.get(0));
 		
 		return modelo.getBotao();
 	}
 	
 	public int[] getModelLetra(){
 		
-		Collections.shuffle(letra);
+		Collections.shuffle(letter);
 		
-		GameModel modelo = acessar.getModelLetra((String) letra.get(0));
+		GameModel modelo = acessar.getModelLetra((String) letter.get(0));
 		
 		int[] id = new int[2];
 		
@@ -81,9 +81,9 @@ public class GameController {
 	
 	public int getIdBotaoLetra(){
 		
-		Collections.shuffle(letra);
+		Collections.shuffle(letter);
 		
-		GameModel modelo =  acessar.getModelLetra((String) letra.get(0));
+		GameModel modelo =  acessar.getModelLetra((String) letter.get(0));
 		
 		return modelo.getBotao();
 	}
