9fe6127f2b2e3e36a39118e258684a114f82d5f8
camilaferreira <camilaferreira251@gmail.com>
2014-06-02 09:12:27 -0300

    Move class Information for package view

---
+++ ABCDario/src/fga/mds/abcdario/view/Information.java
@@ -0,0 +1,64 @@
+/*Class Information
+ * Abcdario is free software: you can redistribute it and / or
+
+     modify it under the terms of the GNU General Public License as
+
+     published by the Free Software Foundation (FSF), version 2 of the
+
+     License, or (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+
+     but WITHOUT ANY WARRANTY; without even the implied warranty of fitness for any
+
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. View
+
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+
+     along with this program, if not, write to the Foundation of Software
+
+     Free (FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA */
+
+package fga.mds.abcdario.view;
+
+import jogo.AbstractActivity;
+import jogo.FirstActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import fga.mds.abcdario.R;
+
+public class Information extends AbstractActivity {
+
+	private ImageView backButton;
+
+	// This is the first function to be executed when an Activity is launched
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.informacao);
+
+		initializeComponents();
+		defineEvents();
+	}
+	
+	// Initializes the components used in activity
+	public void initializeComponents() {
+		backButton = (ImageView) findViewById(R.imgV.bt_voltar);
+	}
+
+	// Defines the events of activity
+	public void defineEvents() {
+
+		backButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View view) {
+				startActivity(new Intent(Information.this, FirstActivity.class));
+
+			}
+		});
+
+	}
+}
