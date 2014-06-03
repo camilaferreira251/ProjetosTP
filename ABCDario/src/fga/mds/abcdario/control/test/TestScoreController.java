4c2580825508c2ecf7159f6db48ff9a5af6209d5
Jads <jadsvictor@hotmail.com>
2014-06-03 00:09:18 -0300

    Creating ScoreController's tests

--- ABCDario/src/fga/mds/abcdario/control/test/TestScoreController.java
+++ ABCDario/src/fga/mds/abcdario/control/test/TestScoreController.java
@@ -21,4 +21,73 @@ public class TestScoreController {
 	}
 
 
+	@Test
+	public void testDefineImgStars5(){
+		ScoreController test = new ScoreController();
+		int id;
+		
+		id = test.definirImgEstrelas(100);
+		
+		assertEquals(R.drawable.img_5_estrelas, id); 
+	}
+
+	@Test
+	public void testDefineImgStars4(){
+		ScoreController test = new ScoreController();
+		int id;
+		
+		id = test.definirImgEstrelas(70);
+		
+		assertEquals(R.drawable.img_4_estrelas, id);
+	}
+	
+	@Test
+	public void testDefineImgStars3(){
+		ScoreController test = new ScoreController();
+		int id;
+		
+		id = test.definirImgEstrelas(50);
+		
+		assertEquals(R.drawable.img_3_estrelas, id);
+	}
+	
+	@Test
+	public void testDefineImgStars2(){
+		ScoreController test = new ScoreController();
+		int id;
+		
+		id = test.definirImgEstrelas(30);
+		
+		assertEquals(R.drawable.img_2_estrelas, id);
+	}
+	
+	@Test
+	public void testDefineImgStars1(){
+		ScoreController test = new ScoreController();
+		int id;
+		
+		id = test.definirImgEstrelas(15);
+		
+		assertEquals(R.drawable.img_1_estrela, id);
+	}
+	
+	@Test
+	public void testDefineImgStars0(){
+		ScoreController test = new ScoreController();
+		int id;
+		
+		id = test.definirImgEstrelas(0);
+		
+		assertEquals(R.drawable.img_0_estrelas, id);
+	}
+	
+	@Test
+	public void testDefineImageStars(){
+		ScoreController test = new ScoreController();
+		int id;
+		
+		id = test.definirImgEstrelas(-1);
+		
+		assertEquals(R.drawable.img_0_estrelas, id);
+	}
 }
