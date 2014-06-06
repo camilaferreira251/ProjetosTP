24ecc7d8912e9a0ffbfc0e96b2078c74cde30e63
carol15022 <carol15022@hotmail.com>
2014-06-05 22:33:17 -0300

    Creating testGetInstance in TestLearnPersistence

--- ABCDario/src/fga/mds/abcdario/persistencia/test/TestLearnPersistence.java
+++ ABCDario/src/fga/mds/abcdario/persistencia/test/TestLearnPersistence.java
@@ -9,6 +9,12 @@ import fga.mds.abcdario.model.LearnModel;
 import fga.mds.abcdario.persistencia.LearnPersistence;
 
 public class TestLearnPersistence {
-    
+	@Test
+	public void testGetInstance() {
+    	LearnPersistence instancia = LearnPersistence.getInstance();
+    	LearnPersistence instancia2 = LearnPersistence.getInstance();
+		
+		assertEquals(instancia, instancia2);
+	}
 
 }
