f148cf792a679be973e70f1e2e0156ea7faef2cc
carol15022 <carol15022@hotmail.com>
2014-06-05 23:57:51 -0300

    Add TestCases in TestSuit

--- ABCDario/src/jogo/TestSuit.java
+++ ABCDario/src/jogo/TestSuit.java
@@ -6,10 +6,13 @@ import org.junit.runners.Suite.SuiteClasses;
 import fga.mds.abcdario.model.test.TestGameModel;
 import fga.mds.abcdario.model.test.TestLearnModel;
 import fga.mds.abcdario.model.test.TestScoreModel;
+import fga.mds.abcdario.persistencia.test.TestGamePersistence;
+import fga.mds.abcdario.persistencia.test.TestLearnPersistence;
+import fga.mds.abcdario.persistencia.test.TestScorePersistence;
 
 
 @RunWith(Suite.class)
-@SuiteClasses({TestLearnModel.class, TestGameModel.class, TestScoreModel.class})
+@SuiteClasses({TestLearnModel.class, TestGameModel.class, TestScoreModel.class, TestScorePersistence.class, TestLearnPersistence.class, TestGamePersistence.class})
 public class TestSuit {
 
 }
