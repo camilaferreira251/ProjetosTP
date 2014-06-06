dea8e76ac4fc580f7b3f6316bceee83144eeedf1
carol15022 <carol15022@hotmail.com>
2014-06-05 23:40:47 -0300

    Creating testScorePersistence in TestScorePersistence

--- ABCDario/src/fga/mds/abcdario/persistencia/test/TestScorePersistence.java
+++ ABCDario/src/fga/mds/abcdario/persistencia/test/TestScorePersistence.java
@@ -1,14 +1,18 @@
 package fga.mds.abcdario.persistencia.test;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import fga.mds.abcdario.R;
+import fga.mds.abcdario.persistencia.ScorePersistence;
 
 public class TestScorePersistence {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testScorePersistencia() {
+		ScorePersistence firstTest = ScorePersistence.getInstance();
+		ScorePersistence secondTest = ScorePersistence.getInstance();
+		
+		assertEquals(firstTest, secondTest);
 	}
 
 }
