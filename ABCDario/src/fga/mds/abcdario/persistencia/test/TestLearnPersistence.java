3fc542a7ebf0a8d36274b5a0d74d20442c253d1a
carol15022 <carol15022@hotmail.com>
2014-06-05 22:27:26 -0300

    Creating test for LearnPesistence

---
+++ ABCDario/src/fga/mds/abcdario/persistencia/test/TestLearnPersistence.java
@@ -0,0 +1,14 @@
+package fga.mds.abcdario.persistencia.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fga.mds.abcdario.R;
+import fga.mds.abcdario.model.LearnModel;
+import fga.mds.abcdario.persistencia.LearnPersistence;
+
+public class TestLearnPersistence {
+    
+
+}
