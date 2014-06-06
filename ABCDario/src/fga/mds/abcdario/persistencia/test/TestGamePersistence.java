e6f485bf1f50b12a70385fad6b67208e7cff51a2
carol15022 <carol15022@hotmail.com>
2014-06-05 23:15:00 -0300

    Creating testGetInstance in TestGamePersistence

--- ABCDario/src/fga/mds/abcdario/persistencia/test/TestGamePersistence.java
+++ ABCDario/src/fga/mds/abcdario/persistencia/test/TestGamePersistence.java
@@ -1,14 +1,21 @@
 package fga.mds.abcdario.persistencia.test;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import fga.mds.abcdario.R;
+import fga.mds.abcdario.model.GameModel;
+import fga.mds.abcdario.persistencia.GamePersistence;
+
 
 public class TestGamePersistence {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test	
+	public void testGetInstance() {
+		
+		GamePersistence firstInstance = GamePersistence.getInstance();
+		GamePersistence secondInstance = GamePersistence.getInstance();
+		
+		assertEquals(firstInstance, secondInstance);
 	}
 
 }
